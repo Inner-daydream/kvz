06332d3a4fe5252747b3747b783a3b4e6f08e4c8
Lucas <contact@lucasquitman.fr>
2024-02-13 22:36:16 +0100

    removing debugging statements from main.go

--- cmd/main.go
+++ cmd/main.go
@@ -12,22 +12,19 @@ import (
 
 func main() {
 	// TODO: store the db in a better/user defined place
-	db, err := sqlite.OpenDB("kv.db")
+	dbPath := "kv.db"
+	db, err := sqlite.OpenDB(dbPath)
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = sqlite.Migrate(db)
-	if err != nil {
-		log.Fatal(err)
-	}
-	var repo kv.KvRepository = sqlite.New(db)
+	defer db.Close()
+	sqlite.Migrate(db)
+	queries := sqlite.New(db)
+	repo := sqlite.NewRepository(queries)
 	var service kv.KvService = kv.NewServcice(repo)
-	cliCommands := cli.NewCli(&service)
+	if err != nil {
+		log.Fatal(err)
+	}
+	cliCommands := cli.NewCli(service)
 	cli.ParseAndExecute(cliCommands)
-	// service.Set("path", "this/is/my/path")
-	// val, err := service.Get("path")
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-	// fmt.Printf("Fetched val is: %s", val)
 }
